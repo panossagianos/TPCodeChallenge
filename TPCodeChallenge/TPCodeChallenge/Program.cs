91c5e34081f83c75e79be967156a98aef4d7560f
panossagianos <panos_sagianos@hotmail.com>
2017-05-30 11:30:09 +0300

    Add files via upload

---
+++ TPCodeChallenge/TPCodeChallenge/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace TPCodeChallenge
+{
+    class Program
+    {
+        #region Global variables.
+        /// <summary>
+        /// The weighted string version of the checkPrase.
+        /// </summary>
+        private static WeighedString checkPhrase = new WeighedString(ConfigurationManager.AppSettings.Get("checkPhrase"));
+        /// <summary>
+        /// The easy md5 hash to test the final phrases with to determine the answer.
+        /// </summary>
+        private static string checkHashEasy = ConfigurationManager.AppSettings.Get("checkHashEasy");
+        /// <summary>
+        /// The medium md5 hash to test the final phrases with to determine the answer.
+        /// </summary>
+        private static string checkHashMedium = ConfigurationManager.AppSettings.Get("checkHashMedium");
+        /// <summary>
+        /// The hard md5 hash to test the final phrases with to determine the answer.
+        /// </summary>
+        private static string checkHashHard = ConfigurationManager.AppSettings.Get("checkHashHard");
+        /// <summary>
+        /// The number of answers found currently by the application.
+        /// </summary>
+        private static int answersFound = 0;
+        #endregion
+
+        /// <summary>
+        /// Reads from the configuration file a phrase, a words list and three MD5 hash values.
+        /// Then produces all possible anagrams of that phrase.
+        /// Finally, it displays the phrases that correspond to the given MD5 hash values (if any).
+        /// </summary>
+        static void Main()
+        {
+            string wordListFileLocation = ConfigurationManager.AppSettings.Get("wordListLocation");
+            List<string> wordList = File.ReadAllLines(wordListFileLocation).ToList();
+
+            Console.WriteLine("The initial word list contains " + wordList.Count.ToString() + " words.");
+            wordList = RemoveInvalidAndDuplicateWords(wordList, checkPhrase);
+            Console.WriteLine("The word list after removing invalid words contains " + wordList.Count.ToString() + " words.");
+            // Default assumption of a maximum of 4 word anagrams. (next region enables user defined depth searches)
+            int noOfWords = 0;
+            int.TryParse(ConfigurationManager.AppSettings.Get("defaultNoOfWords"), out noOfWords);
+
+            #region User specified words search.
+            //// The user may choose the maximum depth of recursion in order to improve performance if he knows the number of words
+            //// contained in the actual solution.
+            //string userInput = "";
+            //do
+            //{
+            //    Console.WriteLine("");
+            //    Console.WriteLine("Please choose a maximum number of words for the searched anagrams.");
+            //    Console.WriteLine("WARNING: Any number of words over 3 will take considerably more time to complete.");
+            //    Console.WriteLine("");
+            //    Console.WriteLine("Number of words (leave empty if none specified): ");
+            //    userInput = Console.ReadLine();
+            //    if (string.IsNullOrEmpty(userInput))
+            //    {
+            //        noOfWords = checkPhrase.Phrase.Replace(" ", "").Length;  // When the user does not specify a number of words then choose
+            //        userInput = noOfWords.ToString();                        // the length of the initial phrase assuming that the solution could be
+            //    }                                                            // a combination of 1 character words.
+            //    if (!int.TryParse(userInput, out noOfWords))
+            //    {
+            //        Console.WriteLine("");
+            //        Console.WriteLine("The number of words must be an integer number greater than zero.");
+            //    }
+            //} while (noOfWords < 1);
+            #endregion
+
+            // The anagram search takes place.
+            Console.WriteLine("");
+            Console.WriteLine("Processing started...." + DateTime.Now.ToShortTimeString());
+            CheckWordList(wordList, checkPhrase, noOfWords);
+            Console.WriteLine("Processing ended...." + DateTime.Now.ToShortTimeString());
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Removes all words from a list that could not be parts of the specified character weight
+        /// and limits appearances of words to one per word.
+        /// </summary>
+        /// <param name="sourceList">The list that is going to be filtered.</param>
+        /// <param name="sourceWeight">The character weight to compare the list to.</param>
+        /// <returns></returns>
+        private static List<string> RemoveInvalidAndDuplicateWords(List<string> sourceList, WeighedString sourceWeighedString)
+        {
+            List<string> result = new List<string>();
+
+            foreach (string word in sourceList)
+            {
+                WeighedString weighedWord = word.ToWeighedString();
+                if (weighedWord.IsSubSetOf(sourceWeighedString)
+                    && !result.Contains(word))
+                    result.Add(word);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Searches all possible anagrams of the specified wordList (of the specified number of words) that have
+        /// the same character weight as that of the checkString. For those that satisfy the criteria it makes an md5 hash check against
+        /// the three possible hashes specified in the application settings.
+        /// </summary>
+        /// <param name="wordList">The input wordList to check for possible solutions.</param>
+        /// <param name="checkString">The string to base the comparison on.</param>
+        /// <param name="numberOfWords">The number of words in the anagrams.</param>
+        private static void CheckWordList(List<string> wordList, WeighedString checkString, int numberOfWords)
+        {
+            // After each iteration all anagrams containing the checked word have been processed.
+            // The wordListCopy helps us remove processed words from it without affecting the iteration.
+
+            wordList = wordList.OrderByDescending(x => x.Length).ToList();
+            List<string> wordListCopy = new List<string>(wordList);
+            foreach (string word in wordList)
+            {
+                if (answersFound > 2)  // All answers have been found.
+                    return;
+                wordListCopy.Remove(word);
+                CheckAnagramsBasedOnWord(word, wordListCopy, checkString, numberOfWords);
+            }
+        }
+
+        /// <summary>
+        /// Used internally to produce and check for a solution on all possible anagramms that contain the baseWord.
+        /// </summary>
+        /// <param name="baseWord">The word (or phrase) that is going to be checked for solutions.</param>
+        /// <param name="wordList">The rest of the word list to make combinations with.</param>
+        /// <param name="checkString">The comparison phrase.</param>
+        /// <param name="numberOfWords">The maximum number on which the recursive search will take place.</param>
+        private static void CheckAnagramsBasedOnWord(string baseWord, List<string> wordList, WeighedString checkString, int numberOfWords)
+        {
+            if (answersFound > 2) // All answers have been found.
+                return;
+
+            // The function always attempts to hash check word combinations to the baseWord that have the same character weight to the
+            // checkString.
+            List<string> checkWords = wordList.FindAll(x => (baseWord.ToWeighedString() + x.ToWeighedString()) == checkString);
+            foreach (string word in checkWords)
+                HashCheck(baseWord + " " + word);
+
+            // Then it adds words to the current base word to explore anagramms of the combination (subsets) using recursion.
+            if (baseWord.Count(x => x == ' ') < (numberOfWords - 2))  // Max recursion limitation to improve performance.
+            {
+                List<string> eligibleWords = wordList.FindAll(x => (baseWord.ToWeighedString() + x.ToWeighedString()).IsSubSetOf(checkString));
+                List<string> eligibleWordsCopy = new List<string>(eligibleWords);
+                eligibleWords = eligibleWords.OrderBy(x => x.Length).ToList();
+                foreach (string word in eligibleWords)
+                {
+                    if (word.Length > (checkString.Phrase.Length - baseWord.Length + 1))  // Since the list is sorted the remaining word combinations
+                        return;                                                           // will always produce larger phrases than the checkString.
+                    eligibleWordsCopy.Remove(word);
+                    CheckAnagramsBasedOnWord(baseWord + " " + word, eligibleWordsCopy, checkString, numberOfWords);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Compares the MD5 hash of the given checkPhrase and all of its space based
+        /// permutations to the ones found in the application settings.
+        /// For each answer found a notification message is shown on the screen.
+        /// </summary>
+        /// <param name="checkWord">The word to be hash checked.</param>
+        private static void HashCheck(string checkWord)
+        {
+            List<string> checkPerms = checkWord.GetAllPermutations(' ');
+            foreach (string word in checkPerms)
+            {
+                string wordHash = GenerateMd5Hash(word);
+                if (wordHash.ToLower() == checkHashEasy)
+                {
+                    Console.WriteLine("The easy answer is: " + word);
+                    answersFound++;
+                }
+                if (wordHash.ToLower() == checkHashMedium.ToLower())
+                {
+                    Console.WriteLine("The medium answer is: " + word);
+                    answersFound++;
+                }
+                if (wordHash.ToLower() == checkHashHard.ToLower())
+                {
+                    Console.WriteLine("The hard answer is: " + word);
+                    answersFound++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Generates the MD5 Hash code of the inputText.
+        /// </summary>
+        /// <param name="inputText">The text to be processed.</param>
+        private static string GenerateMd5Hash(string inputText)
+        {
+            // Calculate the MD5 has from the inputText.
+            MD5 md5 = MD5.Create();
+            byte[] inputTextBytes = Encoding.ASCII.GetBytes(inputText);
+            byte[] hash = md5.ComputeHash(inputTextBytes);
+
+            // Convert the byte array to a hex string.
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+                sb.Append(hash[i].ToString("x2"));
+            return sb.ToString();
+        }
+    }
+}
---
+++ TPCodeChallenge/TPCodeChallenge/WeighedString.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TPCodeChallenge
+{
+    /// <summary>
+    /// Represents a string acompanied by its character weight.
+    /// Character weights are sets of each character present in the current string acompanied by the number
+    /// of times it appears in it. Spaces are not included when weighing a string.
+    /// </summary>
+    public class WeighedString
+    {
+        /// <summary>
+        /// The string representation of the weighted string.
+        /// </summary>
+        public string Phrase { get; set; }
+
+        /// <summary>
+        /// The character weight of the weighted string.
+        /// </summary>
+        public Dictionary<char, int> Weight { get; }
+
+        /// <summary>
+        /// The number of spaces present in the Phrase property.
+        /// </summary>
+        public int SpaceCount { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the WeighedString class setting it's Phrase property.
+        /// </summary>
+        /// <param name="phrase">The Phrase property of this instance.</param>
+        public WeighedString(string phrase)
+        {
+            Phrase = phrase;
+            Weight = GetCharWeight(phrase);
+            SpaceCount = Phrase.Count(x => x == ' ');
+        }
+
+        /// <summary>
+        /// Returns the set of characters in the specified text and how many times each one appears in it.
+        /// Note: Spaces are not included in the returned set.
+        /// </summary>
+        /// <param name="inputText">The text for which the character weight will be returned.</param>
+        /// <returns></returns>
+        private static Dictionary<char, int> GetCharWeight(string inputText)
+        {
+            Dictionary<char, int> charWeight = new Dictionary<char, int>();
+            inputText = inputText.Replace(" ", "");
+            foreach (char character in inputText)
+            {
+                if (charWeight.ContainsKey(character))
+                {
+                    int weight = 0;
+                    charWeight.TryGetValue(character, out weight);
+                    weight++;
+                    charWeight[character] = weight;
+                }
+                else
+                    charWeight.Add(character, 1);
+            }
+            return charWeight;
+        }
+
+        /// <summary>
+        /// Determines if the current WeighedString's Weight is a subset of the target WeighedString's Weight.
+        /// The WeighedString is a subset even when it has the exact same Weight as the one being compared to.
+        /// </summary>
+        /// <param name="compareTo">The WeighedString to compare this instance to.</param>
+        /// <returns></returns>
+        public bool IsSubSetOf(WeighedString compareTo)
+        {
+            if (Weight.Count > compareTo.Weight.Count) return false;
+
+            // If the current WeighedString contains a character not found in the compareTo or
+            // if it contains a character more times then it's not a subset.
+            foreach (char currWeightKey in Weight.Keys)
+            {
+                if (!compareTo.Weight.ContainsKey(currWeightKey)
+                    || Weight[currWeightKey] > compareTo.Weight[currWeightKey]) return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Two weighed strings are considered equal when their weights match.
+        /// </summary>
+        /// <param name="a">The first item of the comparison.</param>
+        /// <param name="b">The second item of the comparison.</param>
+        public static bool operator ==(WeighedString a, WeighedString b)
+        {
+            // Evaluate the variety of characters.
+            if (a.Weight.Count != b.Weight.Count) return false;
+
+            foreach (char currWeightKey in a.Weight.Keys)
+            {
+                if (!b.Weight.ContainsKey(currWeightKey)
+                    || a.Weight[currWeightKey] != b.Weight[currWeightKey]) return false;
+            }
+            return true;
+        }
+
+        public static bool operator !=(WeighedString a, WeighedString b)
+        {
+            return !(a == b);
+        }
+
+        public static WeighedString operator +(WeighedString a, WeighedString b)
+        {
+            return new WeighedString(a.Phrase + " " + b.Phrase);
+        }
+    }
+
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Converts the current string to a WeighedString.
+        /// </summary>
+        /// <param name="sourceString">The string to be converted.</param>
+        /// <returns></returns>
+        public static WeighedString ToWeighedString(this string sourceString)
+        {
+            return new WeighedString(sourceString);
+        }
+
+        #region GetAllPermutations.
+        /// <summary>
+        /// Produces a list with all possible permutations of the current string based on the
+        /// specified character.
+        /// </summary>
+        /// <param name="input">The string to be permuted.</param>
+        /// <param name="separator">The separator the permutations will be based on.</param>
+        public static List<string> GetAllPermutations(this string input, char separator)
+        {
+            List<string> perms = input.Split(separator).ToList<string>();
+            return Permute(perms);
+        }
+
+        /// <summary>
+        /// Used internally by GetAllPermutations to initiate recursion and produce all permutations of the specified list.
+        /// </summary>
+        /// <param name="input">A list with words that will be permuted.</param>
+        private static List<string> Permute(List<string> input)
+        {
+            // The recursive permute begins with the input list and two index numbers:
+            // that of the first and the last element of the list.
+            int lastIndex = input.Count - 1;
+            return Permute(input, 0, lastIndex);
+        }
+
+        /// <summary>
+        /// Used internally by Permute to carry recursion and populate the list with all possible permutations of the input list.
+        /// </summary>
+        /// <param name="input">The list to be permuted.</param>
+        /// <param name="startIndex">The index to start the current instance of the permutation process.</param>
+        /// <param name="endIndex">The index to end the current instance of the permucation process.</param>
+        private static List<string> Permute(List<string> input, int startIndex, int endIndex)
+        {
+            List<string> result = new List<string>();
+            
+            // When recursion reaches the point where the last word is examined
+            // it returns the combination of words in the order that it was produced by the current iteration.
+            if (startIndex == endIndex)
+            {
+                string currentPermutation = "";
+                foreach (string word in input)
+                {
+                    currentPermutation += " " + word;
+                }
+                result.Add(currentPermutation.Trim());
+            }
+            else  // When iterating we move from start to end swapping the first element with that of the iteration.
+                for (int i = startIndex; i <= endIndex; i++)
+                {
+                    input.Swap(startIndex, i);
+                    result.AddRange(Permute(input, startIndex + 1, endIndex));  // Recursion is used to repeat the process for all elements after
+                    input.Swap(startIndex, i);                                  // that of the current iteration.
+                }
+            return result;
+        }
+        #endregion
+
+        /// <summary>
+        /// Swaps two elements of the current List.
+        /// </summary>
+        /// <typeparam name="T">The type of the list elements.</typeparam>
+        /// <param name="list">The current list.</param>
+        /// <param name="indexA">The index of the first element to be swapped.</param>
+        /// <param name="indexB">The index of the second element to be swapped.</param>
+        public static IList<T> Swap<T>(this IList<T> list, int indexA, int indexB)
+        {
+            T tmp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = tmp;
+            return list;
+        }
+    }
+}
